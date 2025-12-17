0932b9519c77f7c07c615e7a956c6182093e6cd0
khh-Niloy <khhniloy0@gmail.com>
2025-12-17 23:35:10 +0600

    chore: update Next.js to version 16.0.10 and modify image configuration to use remote patterns for better security and flexibility

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,28 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["localhost", "127.0.0.1", "raizmart.com", "raizmart.com.bd", "res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "localhost",
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1",
+      },
+      {
+        protocol: "https",
+        hostname: "raizmart.com",
+      },
+      {
+        protocol: "https",
+        hostname: "raizmart.com.bd",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ],
   },
 };
 
